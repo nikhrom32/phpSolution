59dfb772da175d708172c4b0e4b32c1ed997251b
nikhrom32 <nikhrom32@gmail.com>
2015-05-07 15:18:19 +0600

    Create profile.routes.php

---
+++ profile.routes.php
@@ -0,0 +1,14 @@
+<?php
+$r = Router::Instances();
+
+//$r->get('^\/profile(\/?)$', 'profile_list');
+//$r->get('^\/profile\/(\d+)$', 'profile_view');
+//$r->get('^\/profile\/(\:[^0-9]*)$', 'profile_memberName');
+$r->get('\/profile\/([^\/]+)$', 'profile_memberName');
+
+
+//$r->post('^\/profile\/(\d+)\/sum$', 'profile_sum');
+//$r->post('^\/profile\/(\d+)\/divide$', 'profile_divide');
+//$r->post('^\/profile\/(\d+)\/subtract$', 'profile_subtract');
+//$r->post('^\/profile\/(\d+)\/multiply$', 'profile_multiply');
+?>
