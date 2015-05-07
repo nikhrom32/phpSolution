ebb9e48085491c157b1084d1fa7fa43a77dba187
nikhrom32 <nikhrom32@gmail.com>
2015-05-07 15:15:58 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once 'Router.php';
+include_once 'profile.routes.php';
+include_once 'home.routes.php';
+include_once 'contactUs.routes.php';
+//modules_load_routes();//call {module}.routes.php
+$r = Router::Instances();
+$r->process($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+
+
+function home(){
+	print('Welcome to the home page.');
+}
+
+function profile_memberName($profName){
+	print('I\'m viewing ' . $profName[2] . '\'s profile.');
+}
+
+function contact_us(){
+	print('This action will be fired only if a POST request will occure');
+}
+?> 
